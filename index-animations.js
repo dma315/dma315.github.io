42e631d94c5cee535f79b19876727f27a1819e3f
dma315 <dma315@gmail.com>
2015-12-16 17:34:02 -0600

    Fix mistakes

--- index-animations.js
+++ index-animations.js
@@ -9,29 +9,29 @@ var links = {
 }
 
 $("div.hover-box").mouseenter(
-  function() {
-    var linkKey = $("div.index-tile",this).attr('id');
-    $("div.table-cell", this).html('<a href="' + links[linkKey][1] + '"><div class="index-button">' + links[linkKey][0] + '</div></a>');
-    $("div.opacity", this).css({"opacity": "0.2"});
-    $("div.index-tile", this).css({
-      "-webkit-transform": "scale(1.1, 1.1)",
-      "-moz-transform": "scale(1.1, 1.1)",
-      "-ms-transform": "scale(1.1, 1.1)",
-      "-o-transform": "scale(1.1, 1.1)",
-      "transform": "scale(1.1, 1.1)"
-    })
-  });
+    function() {
+      var linkKey = $("div.index-tile",this).attr('id');
+      $("div.table-cell", this).html('<a href="' + links[linkKey][1] + '"><div class="index-button">' + links[linkKey][0] + '</div></a>');
+      $("div.opacity", this).css({"opacity": "0.2"});
+      $("div.index-tile", this).css({
+        "-webkit-transform": "scale(1.1, 1.1)",
+        "-moz-transform": "scale(1.1, 1.1)",
+        "-ms-transform": "scale(1.1, 1.1)",
+        "-o-transform": "scale(1.1, 1.1)",
+        "transform": "scale(1.1, 1.1)"
+      })
+    });
 
 $("div.hover-box").mouseleave(
-  function() { 
-    var linkKey2 = $("div.index-tile",this).attr('id');
-    $("a", this).html('<p>' + links[linkKey2][0] + '</p>');
-    $("div.opacity", this).css({"opacity": "0.6"});
-    $("div.index-tile", this).css({
-      "-webkit-transform": "scale(1.0, 1.0)",
-      "-moz-transform": "scale(1.0, 1.0)",
-      "-ms-transform": "scale(1.0, 1.0)",
-      "-o-transform": "scale(1.0, 1.0)",
-      "transform": "scale(1.0, 1.0)"
+    function() { 
+      var linkKey2 = $("div.index-tile",this).attr('id');
+      $("a", this).html('<p>' + links[linkKey2][0] + '</p>');
+      $("div.opacity", this).css({"opacity": "0.6"});
+      $("div.index-tile", this).css({
+        "-webkit-transform": "scale(1.0, 1.0)",
+        "-moz-transform": "scale(1.0, 1.0)",
+        "-ms-transform": "scale(1.0, 1.0)",
+        "-o-transform": "scale(1.0, 1.0)",
+        "transform": "scale(1.0, 1.0)"
+      });
     });
-  });
